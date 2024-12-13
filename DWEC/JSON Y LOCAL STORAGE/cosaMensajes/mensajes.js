d30ecdd805fe0a4417c01c76d470da6a67ae5de4
Pablo Pollos <pablo.poligl.1@educa.jcyl.es>
2024-12-13 14:10:32 +0100

    13/12/2024

--- DWEC/JSON Y LOCAL STORAGE/cosaMensajes/mensajes.js
+++ DWEC/JSON Y LOCAL STORAGE/cosaMensajes/mensajes.js
@@ -1,5 +1,48 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded',event => {
+    document.getElementById("enviar").addEventListener('click',guardarMensaje);
+    document.getElementById("borrar").addEventListener('click',borrarLog);
     
+    let array;
+    if(localStorage.getItem("log")){
+        array = JSON.parse(localStorage.getItem("log"));;
+    }else{
+        array = [];
+    }
+    //localStorage.setItem("log",JSON.stringify(array));
+    //Load mensajes
+
+    let table = document.getElementById("barra_mensajes");
+
+    for (let index = 0; index < array.length; index++) {
+        let div = document.createElement("div");
+        if(index%2==0){
+            div.setAttribute("class","yo");
+        }else{
+            div.setAttribute("class","otro");
+        }
+        console.log(array[index]);
+        div.textContent=array[index];
+        table.appendChild(div);
+    }
+
+
 });
+
+
+
+
+function guardarMensaje(){
+    let array = JSON.parse(localStorage.getItem("log"));
+    let fecha = new Date();
+    console.log("fecha");
+    array.push(document.getElementById("user").value+" ["+fecha.toISOtring()+"] - "+document.getElementById("mensaje").value);
+    //array.push(document.getElementById("user").value+" ["+date.toISOtring()+"] - "+document.getElementById("mensaje").value);
+    localStorage.setItem("log",JSON.stringify(array));
+}
+
+function borrarLog() {
+    localStorage.setItem("log",JSON.stringify([]));
+    window.location.href = "index.html";
+}
